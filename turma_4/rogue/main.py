6231145120ac9a671229933c6cef54bf860113a3
Victor Machado da Silva <victor0machado@gmail.com>
2024-05-10 07:59:01 -0300

    Turma 4: Rogue: refatorando main para incluir pygame

--- turma_4/rogue/main.py
+++ turma_4/rogue/main.py
@@ -1,7 +1,18 @@
 from jogo.mecanicas import jogo
 
+import pygame
+
+def iniciar():
+    pygame.init()
+
+def encerrar():
+    print("Saindo do jogo!")
+    pygame.quit()
+
 def main():
+    iniciar()
     jogo()
+    encerrar()
 
 if __name__ == "__main__":
     main()
