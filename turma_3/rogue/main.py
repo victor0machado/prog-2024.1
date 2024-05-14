c3216479a157f4125580b9802268025c26832cc4
Victor Machado da Silva <victor0machado@gmail.com>
2024-05-14 13:42:03 -0300

    Turma 3: Rogue: preparação para a pygame

--- turma_3/rogue/main.py
+++ turma_3/rogue/main.py
@@ -1,7 +1,18 @@
 from jogo.mecanicas.mecanicas import loop
 
+import pygame
+
+def iniciar_jogo():
+    pygame.init()
+
+def encerrar_jogo():
+    print("Saindo do jogo!")
+    pygame.quit()
+
 def main():
+    iniciar_jogo()
     loop()
+    encerrar_jogo()
 
 if __name__ == "__main__":
     main()
